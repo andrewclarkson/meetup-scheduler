b8ee476d799b4f9ba2b3aa6897cff8b72d05f874
Andrew Clarkson <andrew@clarkson.mn>
2014-11-20 14:10:48 -0600

    Added a consumer key

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,8 @@ AUTHORIZE_URL = 'https://secure.meetup.com/oauth2/authorize'
 SERVER_NAME = os.environ['HTTP_HOST']
 
 DEVELOPMENT = os.environ['SERVER_SOFTWARE'].startswith("Development")
-PRODUCTION = !DEVELOPMENT
+PRODUCTION = not DEVELOPMENT
+CONSUMER_KEY = 'k3qsdfp3v7sq6j1job03jajsl1'
 
 if PRODUCTION:
     from app.secret import *
